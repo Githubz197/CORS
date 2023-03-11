54e994e95cda26a979937f9035b741e74207fe62
Githubz197 <127095044+Githubz197@users.noreply.github.com>
2023-03-11 07:46:32 -0800

    Update app.py

--- app.py
+++ app.py
@@ -34,5 +34,14 @@ def proxyy(url):
     headers = {'Content-Type': 'application/json'}
     return Response(response.content, status=response.status_code, headers=headers)
 
+@app.route('/proxx/<path:url>', methods=['GET', 'POST'])
+def proxx(url):
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'}
+    target_headers = {"Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"}
+    payload = "channel=popazik"
+    response = requests.post(url, headers={**headers, **target_headers}, data=payload)
+    headers = {'Content-Type': 'text/html'}
+    return Response(response.content, status=response.status_code, headers=headers)
+
 if __name__ == '__main__':
     app.run()

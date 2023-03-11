71308d847fd796ee0af90078872e72ac884ad59f
Githubz197 <127095044+Githubz197@users.noreply.github.com>
2023-03-11 11:53:58 -0800

    Update app.py

--- app.py
+++ app.py
@@ -12,42 +12,78 @@ def live():
     flash("We are LIVE")
     return render_template('live.html')
 
-@app.route('/proxy/<path:url>')
-def proxy(url):
-    response = requests.get(url)
-    headers = {'Content-Type': 'application/json'}
-    return Response(response.content, status=response.status_code, headers=headers)
+# Font Calvin S - ASCII ART
 
+# ╔═╗┌─┐┌┬┐
+# ║ ╦├┤  │ : Request Get
+# ╚═╝└─┘ ┴ 
+
+#@app.route('/proxy/<path:url>')  THIS WAS THE ORIGINAL, but I changed to /json/:
+@app.route('/json/<path:url>')
+def getJson(url):
+    response = requests.get(url)
+    response_headers = {'Content-Type': 'application/json'}
+    return Response(response.content, status=response.status_code, headers=response_headers)
+
+@app.route('/text/<path:url>')
+def getText(url):
+    response = requests.get(url)
+    response_headers = {'Content-Type': 'text/plain'}
+    return Response(response.content, status=response.status_code, headers=response_headers)
+
+@app.route('/download/<path:url>') # it doesnt know the type to display it so it downloads it?
+def getDownload(url):
+    response = requests.get(url)
+    response_headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+    return Response(response.content, status=response.status_code, headers=response_headers)
+
+
+#Alternative way declaring headers twice adds both to the dictionary "headers"
 @app.route('/html/<path:url>')
 def getHTML(url):
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'}
-    response = requests.get(url, headers=headers)
-    return Response(response.content, status=response.status_code, headers=headers)
+    request_headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'}
+    response = requests.get(url, headers=request_headers)
+    response_headers = {'Content-Type': 'text/html'}
+    return Response(response.content, status=response.status_code, headers=response_headers)
+ 
+@app.route('/notype/<path:url>') # same as HTML but doesnt specify the RETURN RESPONSE type in headers
+def getHTMLL(url):
+    request_headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'}
+    response = requests.get(url, headers=request_headers)
+    return Response(response.content, status=response.status_code)
 
-@app.route('/proxyy/<path:url>', methods=['GET', 'POST'])
-def proxyy(url):
+@app.route('/proxyif/<path:url>', methods=['GET', 'POST']) #both are JSON... redundant?
+def proxyif(url):
     if request.method == 'POST':
         data = request.get_json()
         response = requests.post(url, json=data)
     else:
         response = requests.get(url)
-    headers = {'Content-Type': 'application/json'}
-    return Response(response.content, status=response.status_code, headers=headers)
+    response_headers = {'Content-Type': 'application/json'}
+    return Response(response.content, status=response.status_code, headers=response_headers)
 
-@app.route('/proxx/<path:url>', methods=['GET', 'POST'])
-def proxx(url):
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'}
+
+# ╔═╗┌─┐┌─┐┌┬┐
+# ╠═╝│ │└─┐ │ : Request Post
+# ╩  └─┘└─┘ ┴ 
+
+
+@app.route('/post/<path:url>', methods=['GET', 'POST']) # is this same as HTML?
+def post(url):
+    request_headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'}
+    response = requests.post(url, headers=request_headers)
+    response_headers = {'Content-Type': 'text/html'}
+    return Response(response.content, status=response.status_code, headers=response_headers)
+
+@app.route('/twitch/<path:url>', methods=['GET', 'POST']) # is this same as HTML?
+def twitch(url):
+    request_headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'}
     target_headers = {"Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"}
-    response = requests.post(url, headers={**headers, **target_headers})
-    headers = {'Content-Type': 'text/html'}
-    return Response(response.content, status=response.status_code, headers=headers)
+    payload = "channel=popazik"
+    response = requests.post(url, headers={**request_headers, **target_headers}, data=payload)
+    response_headers = {'Content-Type': 'text/html'}
+    return Response(response.content, status=response.status_code, headers=response_headers)
 
-@app.route('/pronn/<path:url>', methods=['GET', 'POST'])
-def pronn(url):
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'}
-    response = requests.post(url, headers={**headers})
-    headers = {'Content-Type': 'text/html'}
-    return Response(response.content, status=response.status_code)
 
 if __name__ == '__main__':
     app.run()
